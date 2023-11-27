178c13d79a199351d1d1b29f840bfeba2e7b3618
SIkebe <SIkebe@users.noreply.github.com>
2023-11-27 14:52:17 +0900

    download.prompt_for_download

--- src/CookieSample.E2ETests/ChromeFixture.cs
+++ src/CookieSample.E2ETests/ChromeFixture.cs
@@ -34,6 +34,7 @@ public class ChromeFixture : BrowserFixture
         DownloadDir = downloadDir;
 
         opts.AddUserProfilePreference("download.default_directory", DownloadDir);
+        opts.AddUserProfilePreference("download.prompt_for_download", false);
         opts.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
 
         var driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), opts, TimeSpan.FromSeconds(60));
